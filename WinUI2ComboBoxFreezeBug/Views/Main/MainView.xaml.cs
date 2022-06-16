345bf5dcfb647035ba377ede44a8c1063bea4763
Carl Scarlett <carl.orion.scarlett@gmail.com>
2022-06-16 15:07:51 +0800

    First commit

---
+++ WinUI2ComboBoxFreezeBug/Views/Main/MainView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+
+namespace WinUI2ComboBoxFreezeBug.Views.Main
+{
+	public sealed partial class MainView : UserControl
+	{
+		public MainView()
+		{
+			this.InitializeComponent();
+		}
+
+
+
+		//// Event Handlers
+
+
+		async void btnSpawnWindow_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			var newView = new MainView();
+			await TryShowChildWindow(newView);
+		}
+
+
+
+		//// Helpers
+
+
+		public async Task<bool> TryShowChildWindow(UserControl view)
+		{
+			// Get the child window instance
+			var childWindowInstance = await CreateChildWindow();
+			if (childWindowInstance == null)
+				return false;
+
+			// Show the child window
+			var isShown = await childWindowInstance.TryShowAsync();
+			return isShown;
+
+
+			//// Local Functions
+
+
+			async Task<AppWindow> CreateChildWindow(int width = 500, int height = 400)
+			{
+				//Create the child window
+				var newWindow = await AppWindow.TryCreateAsync();
+				if (newWindow == null)
+					return null;
+
+				// Create content
+				var content = view;
+
+				// Set window size
+				var size = new Size(width, height);
+				newWindow.RequestSize(size);
+
+				// Set window content
+				ElementCompositionPreview.SetAppWindowContent(newWindow, content);
+
+				return newWindow;
+			}
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Description
+
+This is a test project replicating a hang issue with `ComboBox` on `AppWindow` with versions of `WinUI 2` after `2.6.0-prerelease.210430001`
+
+# Replication steps
+
+To observe the issue, we will be use the following replication steps:
+
+1. Run the app
+1. Click the button to spawn an AppWindow
+1. On the AppWindow, click the ComboBox
+1. Scrub the mouse pointer across the open Combobox, making sure the mouse goes past the airspace of the open Combobox
+
+# No issues on version `2.6.0-prerelease.210430001`
+
+When running the solution without alteration from the Git repo you are running on WinUI 2 (`Microsoft.UI.XAML` is version `2.6.0-prerelease.210430001`).  In this version of `WinUI` the `ComboBox` does not have an issue.
+
+# Issues on versions after `2.6.0-prerelease.210430001`
+
+Now upgrade the version of `WinUI` to any version after `2.6.0-prerelease.210430001`, run the solution, and repeat the replication steps.  Observe that the hover state stops working and the application hangs.
